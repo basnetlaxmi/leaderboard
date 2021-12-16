733aef7dc7684952fda90370fbb21aa0c50dc674
basnetlaxmi <ddsanzel@gmail.com>
2021-12-16 21:34:01 +0545

    Fix Linter error

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   entry: './src/index.js',
   devServer: {
     static: './dist',
@@ -10,7 +10,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Output Management',
-      template: './src/index.html'
+      template: './src/index.html',
     }),
   ],
   output: {
